e1aa62e01820268e4c71dcb46ab3f2c39c602e73
NishNovae <raecrowned@gmail.com>
2024-09-19 17:51:05 +0900

    2024 09 19 final

---
+++ src/mnist/main.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+# save file
+    img = await file.read()
+    file_name = file.filename
+# check and create dir
+
+    import os
+    upload_dir = os.getcwd() + "/photo/"
+
+    file_full_path = os.path.join(upload_dir, file_name)
+    print(f"DEBUG: file_full_path = {file_full_path}")
+    with open(file_full_path, "wb") as f:
+        f.write(img)
+
+# save file path DB INSERT
+# tablename: image_processing
+# column: num ( auto-incremented, initial insert )
+# column: filename, filepath, req_time ( initial insert ), req_user (n00)
+# column: pred_model, pred_result, pred_time ( to be updated )
+
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "file_full_path": file_full_path
+    }
+
