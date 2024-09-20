907214acf2ca0a64de13865d4d7b0a2fafe981d0
NishNovae <raecrowned@gmail.com>
2024-09-20 11:53:28 +0900

    check

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -1,14 +1,13 @@
 from typing import Annotated
-
 from fastapi import FastAPI, File, UploadFile
+import os
+import pymysql.cursors
 
 app = FastAPI()
-
 
 @app.post("/files/")
 async def create_file(file: Annotated[bytes, File()]):
     return {"file_size": len(file)}
-
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
@@ -17,7 +16,6 @@ async def create_upload_file(file: UploadFile):
     file_name = file.filename
     file_ext = file.content_type.split('/')[-1] # "image/png"
 
-    import os
     upload_dir = os.getcwd() + "/photo/"
     file_full_path = os.path.join(upload_dir, file_name)
 
@@ -34,6 +32,23 @@ async def create_upload_file(file: UploadFile):
 # column: num ( auto-incremented, initial insert )
 # column: filename, filepath, req_time ( initial insert ), req_user (n00)
 # column: pred_model, pred_result, pred_time ( to be updated )
+
+    sql = "INSERT INTO image_processing(file_name, file_path, request_time, request_user) VALUES (%s, %s, %s, %s)"
+    conn = pymysql.connect(
+        host = '172.18.0.1', port = 53306, 
+        user = 'mnist', password = '1234', 
+        database = 'mnistdb', cursorclass=pymysql.cursors.DictCursor
+    )
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql, (
+                file_name, file_full_path, "2024-09-20 11:11:11", "n10"
+                )
+            )
+            conn.commit()
+
+
 
     return {
         "filename": file.filename,
